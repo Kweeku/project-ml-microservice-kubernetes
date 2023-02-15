30b00150767f21610bf5855b141a62592bc133fd
Kweeku <adubenedict10@gmail.com>
2023-02-15 12:44:57 +0000

    Fixed run_kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,7 +8,7 @@ dockerpath="kweeku/udaprojectdemo"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run projectml --image=$dockerpath --port=80
+kubectl run udaprojectdemo --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
@@ -16,4 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward projectml 8000:80
+kubectl port-forward udaprojectdemo 8000:80
